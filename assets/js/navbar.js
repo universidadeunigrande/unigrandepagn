d91bf94baed726574cc274997d483280cc213b96
FranciscoErik <erickrabello56@gmail.com>
2025-07-13 13:10:08 -0300

    Ajusta tamanho e alinhamento da logo na navbar, melhora responsividade

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -6,6 +6,12 @@ const navLinks = document.getElementById('nav-links');
 menuToggle.addEventListener('click', function() {
     menuToggle.classList.toggle('active');
     navLinks.classList.toggle('menu-active');
+    // Adiciona/remover classe para esconder o ícone quando aberto
+    if(navLinks.classList.contains('menu-active')) {
+      menuToggle.classList.add('menu-open');
+    } else {
+      menuToggle.classList.remove('menu-open');
+    }
 });
 
 // Seleciona todos os links de navegação
@@ -14,7 +20,9 @@ const navItems = document.querySelectorAll('#nav-links li a');
 // Adiciona evento de clique em cada item de navegação
 navItems.forEach(item => {
     item.addEventListener('click', () => {
-        // Fecha o menu removendo a classe 'menu-active'
+        // Fecha o menu removendo a classe 'menu-active' e 'menu-open'
         navLinks.classList.remove('menu-active');
+        menuToggle.classList.remove('active');
+        menuToggle.classList.remove('menu-open');
     });
 });
